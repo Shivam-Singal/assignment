fc13d9b23fa688ff296ec0a988f72b9d2e04b1a0
Shivam-Singal <105111820+Shivam-Singal@users.noreply.github.com>
2022-12-26 17:02:46 +0530

    Update download_image.py

--- download_image.py
+++ download_image.py
@@ -1,35 +1,27 @@
 import requests
-#import beautiful soup 4
+
 import bs4
 
 def parse_page(url):
-    response = requests.get(url)
-    #print(response)
+    response = requests.get(url) 
     text = response.text
     soup = bs4.BeautifulSoup(text, "lxml")
     return soup
 
-
 def get_flag_image(soup):
     #select the list of tables with class infobox
-    tables = soup.select('.infobox')
-    #print(tables)
-    #if no table with class infobox is available try class wikitable
+    tables = soup.select('.infobox') 
     if len(tables) == 0:
         tables = soup.select('.wikitable')
 
-    outer_table = tables[0]
-
-    #select the image
+    outer_table = tables[0] 
     index = 0
     image = outer_table.select('img')[index]
-    #print(image)
+    
     while int(image["height"]) < 25 or int(image["width"]) < 25:
         image = outer_table.select('img')[index]
-        index +=1
-    #print(image)
+        index +=1 
     return image
-
 
 def get_image_url(image):
     image_src = image['src']
@@ -43,14 +35,13 @@ def download_and_save_image(image_url, img_name):
     file.write(response.content)
     file.close()
 
-
 #test the functions
 if __name__ == '__main__':
 
-    html_soup = parse_page("https://de.wikipedia.org/wiki/Ägypten")
+    html_soup = parse_page("https://de.wikipedia.org/wiki/india")
 
     image = get_flag_image(html_soup)
 
     url = get_image_url(image)
 
-    download_and_save_image(url, "Ägypten")
+    download_and_save_image(url, "india")
